8f5b479c45d2a45271d3862f1ff83f48efbcd0b0
Vladimir1mir <korneevvladimira@gmail.com>
2025-06-27 16:32:44 +0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,74 +1,69 @@
-# Задача №1:
-
 from pprint import pprint
 
-cook_book = {}
-with open('recipes.txt', 'rt', encoding='utf-8') as file:
-    dishes = ''
-    for i in file:
-        i = i.strip()
-        if i.isdigit():
-            continue
-        elif i and '|' not in i:
-            cook_book[i] = []
-            dishes = i
-        elif i and '|' in i:
-            a, b, c = i.split(" | ")
-            cook_book.get(dishes).append(dict(ingredient_name=a, quantity=int(b), measure=c))
+# Задача №1:
 
+def read_cook_book(filename):
+    cook_book = {}
+    with open(filename, 'r', encoding='utf-8') as file:
+        while True:
+            dish_name = file.readline().strip()
+            if not dish_name:
+                break
+            ingredient_count = int(file.readline().strip())
+            ingredients = []
+            for _ in range(ingredient_count):
+                ingredient_info = file.readline().strip().split(' | ')
+                ingredients.append({
+                    'ingredient_name': ingredient_info[0],
+                    'quantity': int(ingredient_info[1]),
+                    'measure': ingredient_info[2]
+                })
+            cook_book[dish_name] = ingredients
+            file.readline()  # читаем пустую строку между рецептами
+    return cook_book
+
+cook_book = read_cook_book('recipes.txt')
 pprint(cook_book)
 
 # Задача №2:
 
-def get_shop_list_by_dishes(dishes, person_count):
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
     shop_list = {}
     for dish in dishes:
-        if dish in cook_book:
-            for ingredient in cook_book[dish]:
-                if ingredient['ingredient_name'] in shop_list:
-                    shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity'] * person_count
-                else:
-                    shop_list[ingredient['ingredient_name']] = ({'measure': ingredient['measure'], 'quantity':
-                                                                (ingredient['quantity'] * person_count)})
-        else:
-            print('Такого блюда нет в книге')
+        if dish not in cook_book:
+            print(f'Блюдо "{dish}" отсутствует в кулинарной книге')
+            continue
+        for ingredient in cook_book[dish]:
+            name = ingredient['ingredient_name']
+            if name in shop_list:
+                shop_list[name]['quantity'] += ingredient['quantity'] * person_count
+            else:
+                shop_list[name] = {
+                    'measure': ingredient['measure'],
+                    'quantity': ingredient['quantity'] * person_count
+                }
     return shop_list
 
-
-pprint(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2))
+pprint(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2, cook_book))
 
 # Задача №3:
 
-with open('1.txt', 'r', encoding='utf-8') as file_1:
-    line_1 = {}
-    count_1 = 0
-    for line in file_1.readlines():
-        count_1 += 1
-        line_1['1.txt'] = count_1
-with open('1.txt', 'r', encoding='utf-8') as file_1:
-    text_1 = file_1.read()
+def merge_files(files, output_filename):
+    file_info = []
+    for filename in files:
+        with open(filename, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            file_info.append({
+                'name': filename,
+                'line_count': len(lines),
+                'content': ''.join(lines)
+            })
+    
+    file_info.sort(key=lambda x: x['line_count'])
+    
+    with open(output_filename, 'w', encoding='utf-8') as f:
+        for info in file_info:
+            f.write(f"{info['name']}\n{info['line_count']}\n{info['content']}\n")
 
-with open('2.txt', 'r', encoding='utf-8') as file_2:
-    line_2 = {}
-    count_2 = 0
-    for line in file_2.readlines():
-        count_2 += 1
-        line_2['2.txt'] = count_2
-with open('2.txt', 'r', encoding='utf-8') as file_2:
-    text_2 = file_2.read()
-
-with open('3.txt', 'r', encoding='utf-8') as file_3:
-    line_3 = {}
-    count_3 = 0
-    for line in file_3.readlines():
-        count_3 += 1
-        line_3['3.txt'] = count_3
-with open('3.txt', 'r', encoding='utf-8') as file_3:
-    text_3 = file_3.read()
-
-join = sorted(list(line_1.items()) + list(line_2.items()) + list(line_3.items()), key=lambda x: x[1])
-
-with open('result.txt', 'w', encoding='utf-8') as file_result:
-    for line in join:
-        file_result.write(f'{join[0][0]}\n {join[0][1]}\n {text_2}\n {join[1][0]}\n {join[1][1]}\n {text_1}\n'
-                          f'{join[2][0]}\n {join[2][1]}\n {text_3}\n')
+files_to_merge = ['1.txt', '2.txt', '3.txt']
+merge_files(files_to_merge, 'result.txt')
